a4c8032696153091d1e2e315d3c999139b018ca8
dombarnard1 <62134053+dombarnard1@users.noreply.github.com>
2020-11-15 20:58:18 +0100

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,7 +1,7 @@
 var start_button_div = document.getElementById("start_button_div");
 start_button_div.style.display = "none";
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 15, 2020 20:00:00 GMT+01:00").getTime();
+var countDownDate = new Date("Nov 22, 2020 20:00:00 GMT+01:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
